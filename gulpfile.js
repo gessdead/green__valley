0861fd8da6947f7bed63d962fbdf554a93eac238
gessdead <s1991gv@gmail.com>
2016-04-09 23:31:51 +0300

    change file structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 const gulp = require('gulp');
 const less = require('gulp-less');
+const plumberNotifier = require('gulp-plumber-notifier');
 const rename = require('gulp-rename');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -12,6 +13,7 @@ const sourcemaps = require('gulp-sourcemaps');
 // Компиляция LESS
 gulp.task('less', function () {
   return gulp.src('./less/style.less')
+    .pipe(plumberNotifier())
     .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(postcss([
@@ -23,3 +25,8 @@ gulp.task('less', function () {
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest('./css'));
 });
+
+// Вотчер за компиляцией LESS
+gulp.task('watch', function() {
+    gulp.watch('less/**/*.less', ['less']);
+    });
