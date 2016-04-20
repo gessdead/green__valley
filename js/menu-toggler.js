c990b1bedc7e6a19cf12dfa324548af42dd771d2
gessdead <s1991gv@gmail.com>
2016-04-20 17:36:07 +0300

    отверстана главная и список комнат

--- js/menu-toggler.js
+++ js/menu-toggler.js
@@ -3,20 +3,4 @@ $(document).ready(function(){
     e.preventDefault();
     $('#hidden').slideToggle('slow');
   });
-
-  $('.gallery__big-img-item').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.gallery__small-img-item'
-  });
-  $('.gallery__small-img-item').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.gallery__big-img-item',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
-  });
 });
--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,75 @@
-  $('.gallery__big-img-item').slick({
+$(document).ready(function(){
+    // слайдер галереи
+  $('.gallery__big-img-list').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    fade: true,
-    asNavFor: '.gallery__small-img-item'
+    fade: false,
+    cssEase: 'ease-in-out',
+    speed: 400,
+    accessibility: false,
+    arrows: false,
+    asNavFor: '.gallery__small-list'
   });
-  $('.gallery__small-img-item').slick({
-    slidesToShow: 3,
+  $('.gallery__small-img-list').slick({
+    slidesToShow: 5,
     slidesToScroll: 1,
-    asNavFor: '.gallery__big-img-item',
-    dots: true,
+    asNavFor: '.gallery__big-img-list',
+    dots: false,
+    cssEase: 'ease-in-out',
+    speed: 400,
     centerMode: true,
+    accessibility: false,
+    arrows: false,
     focusOnSelect: true
   });
+
+// слайдер отзывов
+  $('.reviews__slider').slick({
+     infinite: true,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     cssEase: 'ease-in-out',
+     speed: 400,
+     responsive: [
+       {
+         breakpoint: 768,
+         settings: "unslick"
+       }
+     ]
+   });  
+
+   var clicks = 2;
+   $('.reviews__btn').on('click', function(e){
+     e.preventDefault();
+     if( (clicks % 2) == 0 ){
+       $('.reviews__slider').slick('unslick'); 
+     } else {
+       $('.reviews__slider').slick({
+         infinite: true,
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         cssEase: 'ease-in-out',
+         speed: 400,
+         responsive: [
+           {
+             breakpoint: 768,
+             settings: "unslick"
+           }
+         ]
+       }); 
+     }
+     ++clicks;
+   });
+
+  //слайдер акций
+  $('.sale-slider__inner').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      cssEase: 'ease-in-out',
+      speed: 400,
+      dots: true
+    });
+});
