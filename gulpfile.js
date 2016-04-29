7a292748f921d10f8d6de29649a929440891f3bd
gessdead <s1991gv@gmail.com>
2016-04-29 03:22:24 +0300

    изменил структуру проекта, теперь модно собираю в bild

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ const htmlhint = require('gulp-htmlhint');
 
 //проверка html кода
 gulp.task('htmlhint', function() {
-  return gulp.src('./html/*.html')
+  return gulp.src('./src/html/*.html')
     .pipe(plumberNotifier())
     .pipe(htmlhint({
       "tag-pair": true,
@@ -23,21 +23,37 @@ gulp.task('htmlhint', function() {
 });
 // Компиляция LESS
 gulp.task('less', function () {
-  return gulp.src('./less/style.less')
+  return gulp.src('./src/less/style.less')
     .pipe(plumberNotifier())
-    .pipe(sourcemaps.init())
+    //.pipe(sourcemaps.init())
     .pipe(less())
     .pipe(postcss([
         autoprefixer({browsers: ['last 2 version']}),
         mqpacker
     ]))
-    .pipe(sourcemaps.write())
+    //.pipe(sourcemaps.write())
+    //.pipe(cleanss())
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('./src/css'));
+});
+
+//собиратель в билд
+gulp.task('bild', function(){
+  gulp.src('./src/css/*.css')
     .pipe(cleanss())
     .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('./css'));
-});
+    .pipe(gulp.dest('./bild/css'));
+  gulp.src('./src/html/*.html')
+    .pipe(gulp.dest('./bild/html'));
+  gulp.src('./src/imgs/**/*.*')
+    .pipe(gulp.dest('./bild/imgs'));
+  gulp.src('./src/js/*.js')
+    .pipe(gulp.dest('./bild/js'));
+  gulp.src('./src/svg/*.svg')
+    .pipe(gulp.dest('./bild/svg'));
+  });
 
 // Вотчер 
 gulp.task('watch', function() {
-    gulp.watch('less/**/*.less', ['less']);
+    gulp.watch('/src/less/**/*.less', ['less']);
     });
---
+++ src/js/slider.js
@@ -0,0 +1,113 @@
+$(document).ready(function(){
+    // слайдер галереи
+  $('.gallery__big-img-list').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    cssEase: 'ease-in-out',
+    speed: 400,
+    accessibility: false,
+    arrows: false,
+    asNavFor: '.gallery__small-list'
+  });
+  $('.gallery__small-img-list').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.gallery__big-img-list',
+    dots: false,
+    cssEase: 'ease-in-out',
+    speed: 400,
+    centerMode: true,
+    accessibility: false,
+    arrows: false,
+    focusOnSelect: true
+  });
+
+// слайдер отзывов
+  $('.reviews__slider').slick({
+     infinite: true,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     cssEase: 'ease-in-out',
+     speed: 400,
+     responsive: [
+       {
+         breakpoint: 768,
+         settings: "unslick"
+       }
+     ]
+   });  
+
+   var clicks = 2;
+   $('.reviews__btn').on('click', function(e){
+     e.preventDefault();
+     if( (clicks % 2) == 0 ){
+       $('.reviews__slider').slick('unslick'); 
+     } else {
+       $('.reviews__slider').slick({
+         infinite: true,
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         cssEase: 'ease-in-out',
+         speed: 400,
+         responsive: [
+           {
+             breakpoint: 768,
+             settings: "unslick"
+           }
+         ]
+       }); 
+     }
+     ++clicks;
+   });
+
+  //слайдер акций
+  $('.sale-slider__inner').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      cssEase: 'ease-in-out',
+      speed: 400,
+      dots: true
+    });
+
+  //слайдер single room
+  $('.about-room__list').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      fade: false,
+      cssEase: 'ease-in-out',
+      speed: 400,
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false
+          }
+        }
+      ]
+    });
+
+  //зумер single room
+  $('.about-room__list-zoom').on('click', function(e){
+      e.preventDefault();
+      var zoomedImg = $(".slick-slide[class*='slick-current']").find('img').attr('src');
+      $('#zoomer img').attr('src', zoomedImg);
+      $('#zoomer').toggle(400);
+      $('.zoomer__inner').toggle(100);
+      $('.about-room__list-zoom').toggle(400);
+      $('.slick-arrow').toggle();
+    });
+
+    $('.zoomer--unzoom').on('click', function(e){
+      e.preventDefault();
+      $('#zoomer').toggle(400);
+      $('.zoomer__inner').toggle(100);
+      $('.about-room__list-zoom').toggle(400);
+      $('.slick-arrow').toggle();
+    });
+});
